09b14d5cb00a786e71d0f9679a798a6bc55fd1d7
Adekunle Aderibigbe <aderibigbe.adekunle21@gmail.com>
2022-11-08 02:45:52 +0000

    Auth with the cloud done

--- app/src/main/java/uk/ac/tees/b1515396/teezapplication/RegisterUser.java
+++ app/src/main/java/uk/ac/tees/b1515396/teezapplication/RegisterUser.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterUser extends AppCompatActivity implements View.OnClickListener{
 
@@ -99,6 +101,31 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             User user = new User(fullName, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(RegisterUser.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+
+                                                progressBar.setVisibility(View.GONE);
+
+                                                // Redirect to login layout!
+                                            }
+                                            else {
+                                                Toast.makeText(RegisterUser.this, "Failed to register! Please try again!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        }
+
+                        else {
+                            Toast.makeText(RegisterUser.this, "Failed to register!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });

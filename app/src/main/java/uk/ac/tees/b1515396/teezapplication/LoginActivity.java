5539b9c614a5ee409ff19ff5abf0c1c2c1dd5f08
Adekunle Aderibigbe <aderibigbe.adekunle21@gmail.com>
2022-11-08 01:04:11 +0000

    Register Layout Created

--- app/src/main/java/uk/ac/tees/b1515396/teezapplication/LoginActivity.java
+++ app/src/main/java/uk/ac/tees/b1515396/teezapplication/LoginActivity.java
@@ -2,13 +2,30 @@ package uk.ac.tees.b1515396.teezapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.tv_register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.tv_register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
     }
 }
